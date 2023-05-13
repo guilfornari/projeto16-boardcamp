33cb4c277254655a89d4c854bd722bbea9dc431d
Guilfornari <guilfornari@gmail.com>
2023-05-13 16:48:49 -0300

    implemented add games to games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -5,11 +5,14 @@ export async function addGame(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body;
 
     try {
-        const game = await db.query(`
-        INSERT INTO games (name, image, "stockTotal", "pricePerDay")
-        VALUES (${name}, ${image}, ${stockTotal}, ${pricePerDay});`);
 
-        console.table(game);
+        const game = await db.query(`SELECT games.name FROM games WHERE name = $1;`, [name]);
+        if (game) return res.sendStatus(409);
+
+        await db.query(`
+                INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+                VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+
         res.sendStatus(201);
 
     } catch (err) {
